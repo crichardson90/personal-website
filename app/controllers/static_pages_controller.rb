899ae97300635d76699bbd59540054b78183c209
Coty Richardson <crichardson90@live.com>
2018-10-03 18:53:16 -0500

    First Commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def about
+    render "about"
+  end
+
+  def contact
+    render "contact"
+  end
+
+  def portfolio
+    render "portfolio"
+  end
+end
